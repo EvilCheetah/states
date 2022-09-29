a45086921d225dc4e713e74e92dde2140a18d58a
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-29 15:00:41 -0700

    initial commit

---
+++ data/cities.ts
@@ -0,0 +1,33 @@
+import { ALABAMA } from "./cities/alabama";
+import { ALASKA } from "./cities/alaska";
+import { ARIZONA } from "./cities/arizona";
+import { ARKANSAS } from "./cities/arkansas";
+import { CALIFORNIA } from "./cities/california";
+import { COLORADO } from "./cities/colorado";
+import { DELAWARE } from "./cities/delaware";
+import { FLORIDA } from "./cities/florida";
+import { GEORGIA } from "./cities/georgia";
+import { HAWAII } from "./cities/hawaii";
+import { IDAHO } from "./cities/idaho";
+
+
+export const CITIES = {
+    ...ALABAMA,
+    ...ALASKA,
+    ...ARIZONA,
+    ...ARKANSAS,
+
+    ...CALIFORNIA,
+    ...COLORADO,
+
+    ...DELAWARE,
+
+    ...FLORIDA,
+
+    ...GEORGIA,
+
+    ...HAWAII,
+    
+    ...IDAHO,
+
+}
---
+++ data/cities/delaware.ts
@@ -0,0 +1,72 @@
+/*
+    Author:         Eugene Moshchyn
+    Scraped Using:  Excel Data Import
+
+    Last Update:    Sep 29th, 2022
+    Source:         https://bit.ly/3xZM4Ni
+
+    Credit:         Wikipedia® | Wikimedia Foundation, Inc.
+*/
+
+export const DELAWARE = {
+    'Delaware':
+    [
+        'Arden',
+        'Ardencroft',
+        'Ardentown',
+        'Bellefonte',
+        'Bethany Beach',
+        'Bethel',
+        'Blades',
+        'Bowers',
+        'Bridgeville',
+        'Camden',
+        'Cheswold',
+        'Clayton',
+        'Dagsboro',
+        'Delaware City',
+        'Delmar',
+        'Dewey Beach',
+        'Dover',
+        'Ellendale',
+        'Elsmere',
+        'Farmington',
+        'Felton',
+        'Fenwick Island',
+        'Frankford',
+        'Frederica',
+        'Georgetown',
+        'Greenwood',
+        'Harrington',
+        'Hartly',
+        'Henlopen Acres',
+        'Houston',
+        'Kenton',
+        'Laurel',
+        'Leipsic',
+        'Lewes',
+        'Little Creek',
+        'Magnolia',
+        'Middletown',
+        'Milford',
+        'Millsboro',
+        'Millville',
+        'Milton',
+        'New Castle',
+        'Newark',
+        'Newport',
+        'Ocean View',
+        'Odessa',
+        'Rehoboth Beach',
+        'Seaford',
+        'Selbyville',
+        'Slaughter Beach',
+        'Smyrna',
+        'South Bethany',
+        'Townsend',
+        'Viola',
+        'Wilmington',
+        'Woodside',
+        'Wyoming',
+    ]
+}
